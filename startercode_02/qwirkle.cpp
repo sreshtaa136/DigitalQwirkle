e8bf9168a5f33cd0de443cf27b562e9671ed3342
DavidAziz3825605 <s3825605@student.rmit.edu.au>
2021-05-07 17:22:57 +0200

    main menu is Done

--- startercode_02/qwirkle.cpp
+++ startercode_02/qwirkle.cpp
@@ -4,12 +4,47 @@
 #include <iostream>
 
 #define EXIT_SUCCESS    0
+using namespace std;
 
 int main(void) {
    LinkedList* list = new LinkedList();
    delete list;
 
-   std::cout << "TODO: Implement Qwirkle!" << std::endl;
+   int choice;
+   cout << " Welcome to Qwirkle!\n";
+   cout << " -------------------\n";
+   cout << " Menu\n";
+   cout << " ----\n";
+   cout << " 1 - New Game\n";
+   cout << " 2 - Load Game\n";
+   cout << " 3 - Credits (Show student information) \n";
+   cout << " 4 - Quit \n";
+   cout << " Enter your: ";
+
+   cin >> choice;
+   switch (choice)
+   {
+   case 1:
+      cout << "game start!\n";
+      // rest of code here
+      break;
+   case 2:
+      cout << "Story so far....\n";
+      // rest of code here
+      break;
+   case 3:
+      cout << "Ahahah, you really think I will help you?\n";
+      // rest of code here
+      break;
+   case 4:
+      cout << "End of Program.\n";
+      break;
+   default:
+      cout << "Not a Valid Choice. \n";
+      cout << "Choose again.\n";
+      cin >> choice;
+      break;
+   }
 
    return EXIT_SUCCESS;
 }
