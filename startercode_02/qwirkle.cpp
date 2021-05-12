4b76dafe856dbd28ce9ce32537a14d7b015ce1ae
DavidAziz3825605 <s3825605@student.rmit.edu.au>
2021-05-12 18:43:53 +0200

    2.4 User Prompt done

--- startercode_02/qwirkle.cpp
+++ startercode_02/qwirkle.cpp
@@ -43,20 +43,25 @@ int main(int argc, char** argv) {
       std::cout << "> ";
 
       std::cin >> choice;
-      if (std::cin.fail () ){
-         std::cout << "\n !Invalid Data Type! \n\n";
-         choice =4;
+   
+      while (std::cin.fail())
+      {
+         std::cout << "Invalid Input" << std::endl;
+         std::cin.clear();
+         std::cin.ignore(256, '\n');
+         std::cout << "> ";
+         std::cin >> choice;
+         ;
       }
-      
       if (choice == 1)
       {
          std::string name1;
          std::string name2;
-
+         std::cout << "\nStarting a New Game \n\n";
+         std::cout << "Enter a name for player 1 (uppercase characters only) \n"
+         << "> ";
          do{
-            std::cout << "\nStarting a New Game \n\n";
-            std::cout << "Enter a name for player 1 (uppercase characters only) \n"
-                        << "> ";
+            std::cout << "> "; 
             std::cin >> name1;
          } while (!validateName(name1));
 
@@ -96,11 +101,11 @@ int main(int argc, char** argv) {
       }
       else if (choice == 4)
       {
-         std::cout << "\nGoodbye \n";
+         std::cout << "Goodbye \n";
       }
       else
       {
-         std::cout << "Not a Valid Choice. \n";
+         std::cout << "\n Not a Valid Choice. \n";
          std::cout << "Choose again.\n\n";
       }
    }
@@ -109,7 +114,7 @@ int main(int argc, char** argv) {
 
 //to run: 
 //make sure you are on starter code dr and run the following in the terminal
-//g++ -Wall -Werror -std=c++14 -O -o qwirkle qwirkle.cpp Tile.cpp Node.cpp LinkedList.cpp
+//g++ -Wall -Werror -std=c++14 -O -o qwirkle qwirkle.cpp Tile.cpp Node.cpp LinkedList.cpp GameBoard.cpp
 //./qwirkle
 
 bool validateName(std ::string name)
@@ -125,3 +130,4 @@ bool validateName(std ::string name)
    }
    return true;
 }
+   
