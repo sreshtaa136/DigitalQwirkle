d9f24ccc13271c53ceeb35927fd24ce2988cd4cc
Tanishpreet Kaur s3825118 <s3825118@student.rmit.edu.au>
2021-05-12 17:52:33 +1000

    started gameboard

---
+++ startercode_02/GameBoard.cpp
@@ -0,0 +1,21 @@
+#include "GameBoard.h"
+
+GameBoard::GameBoard() {
+    std::vector<Tile*> temp;
+    temp.push_back(nullptr);
+    board.push_back(temp);
+}
+
+GameBoard::~GameBoard() {
+
+}
+
+int GameBoard::getSize() {
+
+}
+
+void GameBoard::displayBoard() {
+
+}
+
+
